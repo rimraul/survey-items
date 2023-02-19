e633f3bfb5284de02f2c7db9646b26a6f48babbd
rimraul <90755898+rimraul@users.noreply.github.com>
2023-02-19 20:00:49 +0200

    Update similarity_network.py

--- similarity_network.py
+++ similarity_network.py
@@ -1,6 +1,7 @@
 import pyreadstat
 import numpy as np
 import pandas as pd
+from sentence_transformers import SentenceTransformer
 from pyvis import network as net
 import faiss
 import streamlit as st
